581816ded5b05ba971c4b5f3e4c0b09bc1b4b990
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-01-23 20:43:00 +0100

    feat: implement clean and scalable book controllers with population

---
+++ src/controllers/book.controller.js
@@ -0,0 +1,101 @@
+const Book = require("../models/author.model");
+
+const getAllBooks = async (req, res, next) => {
+  try {
+    const books = await Book.find().populate("author");
+
+    res.status(200).json({
+      success: true,
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getAllBookById = async (req, res, next) => {
+  try {
+    const book = await Book.findById(req.params.id).populate("author");
+
+    if (!book) {
+      return res.status(404).json({
+        success: false,
+        message: "Book not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: book,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createBook = async (req, res, next) => {
+  try {
+    const book = await Book.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      message: "Book created successfully",
+      data: book,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateBook = async (req, res, next) => {
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, red.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!book) {
+      return res.status(404).json({
+        success: false,
+        message: "Book not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: book,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteBook = async (req, res, next) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({
+        success: false,
+        message: "Book not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Book deleted successfully",
+      data: book,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAllBooks,
+  getAllBookById,
+  createBook,
+  updateBook,
+  deleteBook,
+};
