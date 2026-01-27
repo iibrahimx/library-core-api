a4aa48e6b1be3775471128b73c45125015dd2607
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-01-27 19:34:32 +0100

    feat: add role-based authorization for admin-only actions

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -7,13 +7,14 @@ const {
   deleteBook,
 } = require("../controllers/book.controller");
 const protect = require("../middlewares/auth.middleware");
+const authorizeRoles = require("../middlewares/role.middleware");
 
 const router = express.Router();
 
 router.get("/", getAllBooks);
 router.get("/:id", getAllBookById);
-router.post("/", protect, createBook);
-router.put("/:id", protect, updateBook);
-router.delete("/:id", protect, deleteBook);
+router.post("/", protect, authorizeRoles("admin"), createBook);
+router.put("/:id", protect, authorizeRoles("admin"), updateBook);
+router.delete("/:id", protect, authorizeRoles("admin"), deleteBook);
 
 module.exports = router;
